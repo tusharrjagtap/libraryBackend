e6f3d56b4f678679641a3c9b15e45bcb16db59a0
Tushar Rajabhau Jagtap <99384364+tusharrjagtap@users.noreply.github.com>
2022-04-01 00:22:14 +0530

    not required at more

--- src/main/java/com/app/dao/LoginURepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.LoginU;
-
-public interface LoginURepository extends JpaRepository<LoginU, Integer>{
-
-}
